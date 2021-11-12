ed52bcfbbf788b5b7a43cf2b359ce9aa5d73f521
laura <laura@Laura>
2021-11-12 20:17:49 +0100

    Crear los métodos equals y hashCode para esta clase.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -49,4 +49,20 @@ public class Posicion {
 		return columna;
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
 }
