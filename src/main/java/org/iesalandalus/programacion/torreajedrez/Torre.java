01bd1199247a63d50d47ebb0af5120b197340f3c
laura <laura@Laura>
2021-11-12 20:58:17 +0100

    Crear getter y setter

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -5,4 +5,24 @@ public class Torre {
 	private Color color;
 	private Posicion posicion;
 
+	public Color getColor() {
+		return color;
+	}
+
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		}
+		this.color = color;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setPosicion(Posicion posicion) {
+		this.posicion = new Posicion(posicion);
+
+	}
+
 }
