c2f0e9e23a3c6417db0128c6cac58de4acb5424b
laura <laura@Laura>
2021-11-21 16:30:11 +0100

    crear método estático mostrarTorre y el atributo torre

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -2,8 +2,14 @@ package org.iesalandalus.programacion.torreajedrez;
 
 public class MainApp {
 
+	static Torre torre;
+
 	public static void main(String[] args) {
 		System.out.println("kk");
+	}
+
+	private static void mostrarTorre() {
+		System.out.println(torre.toString());
 	}
 
 }
