4ec23ba86f7b025000416e146f30cb72b374c3ef
laura <laura@Laura>
2021-11-21 16:45:18 +0100

    crear método mostrarMenu

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -12,4 +12,13 @@ public class MainApp {
 		System.out.println(torre.toString());
 	}
 
+	private static void mostrarMenu() {
+		System.out.println("MENÚ");
+		System.out.println("1. Crear torre por defecto.");
+		System.out.println("2. Crear una torre de un color");
+		System.out.println("3. Crear una torre de un color en una columna inicial dada ('c' o 'f')");
+		System.out.println("4. Mover la torre");
+		System.out.println("5. Salir");
+
+	}
 }
